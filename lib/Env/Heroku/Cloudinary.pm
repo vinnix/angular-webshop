d7c9a6674adae27f97f6e191d639822ef8ea8865
Jussi Kinnula <jussi.kinnula@frantic.com>
2015-09-01 14:30:16 +0300

    Fix Env::Heroku::Cloudinary undef

--- lib/Env/Heroku/Cloudinary.pm
+++ lib/Env/Heroku/Cloudinary.pm
@@ -6,17 +6,18 @@ use URI;
 sub import {
     my ($self) = @_;
 
-    my $cloudinaryurl = $ENV{CLOUDINARY_URL};
-    $cloudinaryurl =~ s/^\"//;
-    $cloudinaryurl =~ s/\"$//;
-    if ( $cloudinaryurl and $cloudinaryurl =~ s/^cloudinary:// ) {
-        my $url = URI->new( $cloudinaryurl, 'http' );
-        $ENV{CLOUDINARY_CLOUD} = $url->host;
-        ($ENV{CLOUDINARY_API_KEY},$ENV{CLOUDINARY_API_SECRET}) = split ':', $url->userinfo
-            if $url->userinfo;
-    }
+    if (my $cloudinary_url = $ENV{CLOUDINARY_URL}) {
+        $cloudinary_url =~ s/^\"//;
+        $cloudinary_url =~ s/\"$//;
+        if ( $cloudinary_url and $cloudinary_url =~ s/^cloudinary:// ) {
+            my $url = URI->new( $cloudinary_url, 'http' );
+            $ENV{CLOUDINARY_CLOUD} = $url->host;
+            ($ENV{CLOUDINARY_API_KEY}, $ENV{CLOUDINARY_API_SECRET}) = split ':', $url->userinfo
+                if $url->userinfo;
+        }
 
-    return 1;
+        return 1;
+    }
 }
 
 1;
