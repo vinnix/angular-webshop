5e2ef5dbce95b3df10a9e5bab66c5eb7e0300229
Jussi Kinnula <jussi.kinnula@frantic.com>
2015-09-02 11:20:42 +0200

    Fix password utility

--- lib/Util/Password.pm
+++ lib/Util/Password.pm
@@ -2,25 +2,49 @@ package Util::Password;
 use Moose;
 use namespace::autoclean;
 use WebApp;
-use Digest;
+use Crypt::SaltedHash;
 use Data::Dumper;
 
 sub calculate_password {
-    my ($self, $c, $plain) = @_;
+    my ($self, $c, $password) = @_;
+    my $password_type = $c->config->{'Plugin::Authentication'}
+        ->{'siteuser'}->{'credential'}
+        ->{'password_type'};
 
-    my $hashed = Digest->new($c->config->{'Plugin::Authentication'}
+    my $password_salt_len = $c->config->{'Plugin::Authentication'}
         ->{'siteuser'}->{'credential'}
-        ->{'password_hash_type'});
-   
-    $hashed->add($c->config->{'Plugin::Authentication'}
-        ->{'siteuser'}->{'credential'}
-        ->{'password_pre_salt'})
-        ->add($plain)
-        ->add($c->config->{'Plugin::Authentication'}
-        ->{'siteuser'}->{'credential'}
-        ->{'password_post_salt'});
+        ->{'password_salt_len'} || 0;
 
-   return $hashed->b64digest;      
+    print Dumper $password_type;
+
+    if ($password_type and $password_type eq 'salted_hash') {
+        my $csh = Crypt::SaltedHash->new(
+            algorithm => 'SHA-512',
+            salt_len => $password_salt_len,
+        );
+        $csh->add($password);
+        return $csh->generate;
+    } else {
+        return $password;
+    }
 }
+
+sub validate_password {
+    my ($self, $c, $password, $stored_password) = @_;
+    my $password_type = $c->config->{'Plugin::Authentication'}
+        ->{'siteuser'}->{'credential'}
+        ->{'password_type'};
+
+    my $password_salt_len = $c->config->{'Plugin::Authentication'}
+        ->{'siteuser'}->{'credential'}
+        ->{'password_salt_len'} || 0;
+
+    if ($password_type and $password_type eq 'salted_hash') {
+        return Crypt::SaltedHash->validate($stored_password, $password, $password_salt_len);
+    } else {
+        return $password eq $stored_password;
+    }
+}
+
 
 1;
